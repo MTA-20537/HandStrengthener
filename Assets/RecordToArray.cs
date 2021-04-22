80d99f6fe251a132e1df87b71b3636366c5ecd56
kuff <16867891+kuff@users.noreply.github.com>
2021-04-22 11:59:03 +0200

    sdbfg

--- Assets/RecordToArray.cs
+++ Assets/RecordToArray.cs
@@ -1,12 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class RecordToArray : MonoBehaviour
 {
-    List<float> dataArray;
+    public List<float> dataArray;
     float bciValue;
     GameManager gameData;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,15 +17,22 @@ public class RecordToArray : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        recordToArray();
+        //recordToArray();
+        if (gameData.inputWindow == InputWindowState.Closed)
+        {
+            InterpretData();
+        }
     }
 
     public void OnBCIEvent(float value)
     {
-        bciValue = value;
+        if (gameData.inputWindow == InputWindowState.Open)
+        {
+            dataArray.Add(value);
+        }
     }
 
-    public void recordToArray()
+    /*public void recordToArray()
     {
         Debug.Log(gameData.inputWindow);
         if(gameData.inputWindow == InputWindowState.Open) 
@@ -38,14 +45,13 @@ public class RecordToArray : MonoBehaviour
         }
         if (gameData.inputWindow == InputWindowState.Closed)
         {
-            interpretSignal();
+            InterpretData();
         }
 
-    }
+    }*/
 
-    public void interpretSignal()
+    public void InterpretData()
     {
         dataArray.Clear();
     }
-
 }
