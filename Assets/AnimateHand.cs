b8b3fe9e4463c2ebe4d91873755bf7508b068192
stef5949 <stefc5949@gmail.com>
2021-05-01 21:14:23 +0200

    place hand in gamescene and add script AnimateHand

---
+++ Assets/AnimateHand.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateHand : MonoBehaviour
+{
+    public Animator animator;
+    public GameManager gameManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponentInChildren<MeshRenderer>().material.color = new Color(1f, 1f, 1f, 0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameManager.inputWindow == InputWindowState.Open)
+        {
+            animator.SetBool("grab",true);
+            gameObject.GetComponentInChildren<MeshRenderer>().material.color = new Color(1f, 1f, 1f, 1f);
+        }
+        else
+        {
+            animator.SetBool("grab", false);
+            gameObject.GetComponentInChildren<SkinnedMeshRenderer>().material.color = new Color(1f, 1f, 1f, 0f);
+        }
+       
+    }
+}
