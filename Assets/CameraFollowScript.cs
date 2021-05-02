9e2f8038d8834bb271762e37d69e8b31ed31b180
Fabian <fabiandemeter22@gmail.com>
2021-05-03 00:46:00 +0200

    Movement Setup

---
+++ Assets/CameraFollowScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    public GameObject objectToFollow;
+    private Vector3 followOffset;
+
+    void Start()
+    {
+        followOffset = transform.position - objectToFollow.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = followOffset + objectToFollow.transform.position;
+    }
+}
